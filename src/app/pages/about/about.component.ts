223b7982ee7f1b7095068c3cf6272a2a8f6290f1
Kairat Batyrbayev <k.batyrbayev>
2022-11-28 15:23:42 +0600

    fix about

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -10,15 +10,14 @@ import {HeaderComponent} from "../../components/header.component";
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent{
+export class AboutComponent {
   experience = [
-    {job: 'Angular developer', company: 'Satbayev University', date: '2017 - now'},
-    {job: 'Angular developer', company: 'HardCode', date: '2019 - 2020'},
-    {job: 'Programmer', company: 'KazPost', date: '2016 - 2017'}
+    {job: 'Angular developer', company: 'Satbayev University | IDET', date: '2017 - now', link: 'https://idet.kz/'},
+    {job: 'Angular developer(remote)', company: 'HardCode', date: '2019 - 2020', link: 'https://hardcode.kz/'},
+    {job: 'Programmer', company: 'KazPost', date: '2016 - 2017', link: 'https://post.kz/'}
   ];
   education = [
-    {job: 'Bachelor of Computer Science', company: 'Suleyman Demirel University', date: '2011 - 2016'}
+    {job: 'Bachelor of Computer Science', company: 'Suleyman Demirel University', date: '2011 - 2016', link: 'https://sdu.edu.kz/'}
   ];
-  skills = ['Html', ' Css', ' Js', ' Angular2+', ' d3.js', ' Git', ' Figma', ' Bootstrap'];
-
+  skills = ['Html', ' Css', ' Js', ' Angular2+', ' Git', ' Figma', ' Bootstrap'];
 }
